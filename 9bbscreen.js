7d0f4f5ea7ebaf6b4ab27e9cb7b718f5a6831b82
ushayenni <151708074+ushayenni@users.noreply.github.com>
2025-11-23 22:21:19 +0530

    Update 9bbscreen.js

--- 9bbscreen.js
+++ 9bbscreen.js
@@ -1,22 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-function Counter() {
-  
-  const [count, setCount] = useState(0);
-
-  
-  const handleClick = () => {
-    setCount(count + 1); // Update state
-  };
-
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h2>React Screen Update Example</h2>
-      <h3>Count: {count}</h3>
-      <button onClick={handleClick}>Click Me</button>
-    </div>
-  );
+function Home() {
+  return <h3>Home Page</h3>;
 }
 
-export default Counter;
-`
+function About() {
+  return <h3>About Page</h3>;
+}
+
+function Contact() {
+  return <h3>Contact Page</h3>;
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <h2>React Routing Example</h2>
+
+        <Link to="/">Home</Link> | 
+        <Link to="/about"> About</Link> | 
+        <Link to="/contact"> Contact</Link>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
